0314f06d28aa40d027901361a6f1ad68201f6325
ridima <ridima@gmail.com>
2026-05-09 00:15:27 +0530

    added next, prev, coreect answer and wrong answer functionalities along with some css

---
+++ Quiz-app/src/components/questionBankList.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const questionBank = [
+    {
+        id:1, 
+        question: "What is the capital of France?",
+        options: ["Paris", "London", "Berlin", "Madrid"],
+        answer : "Paris"
+    },
+        {
+        id:2, 
+        question: "What is the capital of India?",
+        options: ["New Delhi", "Mumbai", "Kolkata", "Bombay"],
+        answer : "New Delhi"
+    },
+        {
+        id:3, 
+        question: "What is the capital of England?",
+        options: ["Paris", "London", "Berlin", "Madrid"],
+        answer : "London"
+    }
+]
--- Quiz-app/src/components/quiz.jsx
+++ Quiz-app/src/components/quiz.jsx
@@ -1,35 +1,20 @@
 import React from 'react'
 import { useState } from 'react'
+import {questionBank} from './questionBankList'
 export const Quiz = () => {
-const questionBank = [
-    {
-        id:1, 
-        question: "What is the capital of France?",
-        options: ["Paris", "London", "Berlin", "Madrid"],
-        answer : "Paris"
-    },
-        {
-        id:2, 
-        question: "What is the capital of India?",
-        options: ["New Delhi", "Mumbai", "Kolkata", "Bombay"],
-        answer : "New Delhi"
-    },
-        {
-        id:3, 
-        question: "What is the capital of England?",
-        options: ["Paris", "London", "Berlin", "Madrid"],
-        answer : "London"
-    }
-]
+
+
 const[optionSelected, setOptionSelected] = useState("None")
 const[message, setMessage] = useState("")
 const[currentQuestion, setCurrentQuestion] = useState(0)
+const[isSubmitted, setIsSubmitted] = useState(false)
 
 const handleNext =() =>{
     if (currentQuestion < questionBank.length -1){
         setCurrentQuestion(currentQuestion + 1)
         setOptionSelected("None")
         setMessage("")
+        setIsSubmitted(false)
     }else if (currentQuestion === questionBank.length -1){
         setMessage("You have reached the last question")
     }
@@ -40,6 +25,7 @@ const handlePrev =() =>{
         setCurrentQuestion(currentQuestion - 1)
         setOptionSelected("None")
         setMessage("")
+        setIsSubmitted(false)
 
     }else if (currentQuestion ===0){
         setMessage("You Have Reached the First Question")
@@ -51,15 +37,16 @@ const handleSelectOption = (option) => {
 }
 
 const handleSubmit = () =>{
+    setIsSubmitted(true)
     if (optionSelected === questionBank[currentQuestion].answer){
 
         setMessage("Correct Answer")
+        
     } else{
         setMessage( `Correct Answer is ${questionBank[currentQuestion].answer}`)
     }
         
 }   
-
     return (
     <div>
         <h2>Question {currentQuestion + 1}</h2>
@@ -68,7 +55,8 @@ const handleSubmit = () =>{
         {questionBank[currentQuestion].options.map((option,index)=>(
             <button 
             key = {index}
-            onClick = {()=> handleSelectOption(option)} className= "option">
+            onClick = {()=> handleSelectOption(option)} className= {"option" + 
+            (optionSelected ===option? (isSubmitted ? (option==questionBank[currentQuestion].answer?" green":" red"): " selected"): "")}>
             {option}
         </button>
 
@@ -80,9 +68,10 @@ const handleSubmit = () =>{
         <div className = "nav-buttons"> 
             <button 
             onClick = {handlePrev}> Previous</button>
+            <button onClick = {handleSubmit}> Submit</button>
             <button 
             onClick = {handleNext}> Next</button>
-            <button onClick = {handleSubmit}> Submit</button>
+        
         </div>
     </div>
     )
